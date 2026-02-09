ea72da8f6463996ee53b6661a5db43ac170fa50a
root <root@ip-172-31-40-197.ap-south-1.compute.internal>
2026-02-09 14:39:13 +0000

    Day 3: VPC design and terraform plan

--- infra/main.tf
+++ infra/main.tf
@@ -13,3 +13,68 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+#vpc
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name    = "${var.project}-vpc"
+    Project = var.project
+  }
+}
+
+#Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project}-igw"
+  }
+}
+
+#public_subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+
+  tags = {
+    Name = "${var.project}-public-subnet"
+  }
+}
+
+#private_subnet
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "${var.project}-private-subnet"
+  }
+}
+
+#Public_Route_Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project}-public-rt"
+  }
+}
+
+#Route_Table_Association
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
---
+++ infra/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  description = "AWS region"
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "project" {
+  default = "ai-assisted-devops"
+}
+
